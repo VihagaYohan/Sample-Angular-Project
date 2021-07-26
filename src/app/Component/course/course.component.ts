7e6780d7e1ac4f2b1b7a0a6f7f88c5d652179888
VihagaYohan <vihagayohan94@gmail.com>
2021-07-27 00:03:14 +0530

    Angular bindings, events and pipes

--- src/app/Component/course/course.component.ts
+++ src/app/Component/course/course.component.ts
@@ -11,6 +11,18 @@ import {CourseService} from './course.service'
 export class CourseComponent implements OnInit {
   title = 'List of courses';
   courses = ['course 1', 'course 2', 'course 3'];
+  imageUrl= "http://lorempixel.com/100/100";
+  colSpan = 2;
+  isActive=false;
+  email2="me@example.com";
+  course={
+    title:"The complete Angular course",
+    rating:4.9745,
+    students:30123,
+    price:190.95,
+    releaseDate:new Date(2016,3,1)
+  }
+  description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sequi voluptatibus quaerat nihil, facilis eveniet ex recusandae dolores reprehenderit, iste nostrum magni placeat blanditiis consequuntur modi molestias repudiandae. Similique laboriosam dolorum cumque velit culpa tenetur cupiditate commodi eaque totam. Quidem similique tempora repellendus ea unde. Minima inventore temporibus totam. Provident neque asperiores reprehenderit delectus incidunt culpa id reiciendis consequatur repellat!";
 
   constructor(service:CourseService) {
     this.courses = service.getCourses();
@@ -20,5 +32,26 @@ export class CourseComponent implements OnInit {
 
   getTitle() {
     return this.title;
+  }
+
+  onSave($event:Event){
+    $event.stopPropagation(); 
+    console.log("Button was clicked", $event)
+  }
+
+  onDivClicked(){
+    console.log("Div was clicked")
+  }
+
+  onKeyUp(){
+    console.log("Enter was pressed")
+  }
+
+  onKeyUp_1(email:any){
+    console.log(email)
+  }
+
+  onKeyUp_2(){
+    console.log(this.email2)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,14 +10,19 @@ import { CourseComponent } from './Component/course/course.component';
 // services
 import {CourseService} from './Component/course/course.service'
 
+// pipes
+import {SummaryPipe} from './Pipes/summary.pipe'
+
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
+    SummaryPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CourseService
